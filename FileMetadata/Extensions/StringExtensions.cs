871a3823af58da6d6a1318f18453b962824d487b
Adrian Bieliński <adrian.bielinski.pl@gmail.com>
2020-07-09 10:38:24 +0200

    Renamed _illegals to InvalidChars and made it readonly

--- FileMetadata/Extensions/StringExtensions.cs
+++ FileMetadata/Extensions/StringExtensions.cs
@@ -5,11 +5,11 @@ namespace FileMetadata.Extensions
 {
     public static class StringExtensions
     {
-        private static char[] _illegals = Path.GetInvalidFileNameChars();
+        private static readonly char[] InvalidChars = Path.GetInvalidFileNameChars();
         
         public static string JoinForFilePath(this string[] joinThese, string separator = " - ")
         {
-            return new string(string.Join(separator, joinThese).Where(c => !_illegals.Contains(c)).ToArray());
+            return new string(string.Join(separator, joinThese).Where(c => !InvalidChars.Contains(c)).ToArray());
         }
     }
 }
