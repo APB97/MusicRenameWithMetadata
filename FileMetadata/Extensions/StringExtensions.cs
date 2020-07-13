8c0c6389983a2aefa0f7ecd442e51564802c82f1
Adrian Bieliński <adrian.bielinski.pl@gmail.com>
2020-07-13 18:19:42 +0200

    Add xml summary in StringExtensions.cs

--- FileMetadata/Extensions/StringExtensions.cs
+++ FileMetadata/Extensions/StringExtensions.cs
@@ -7,6 +7,12 @@ namespace FileMetadata.Extensions
     {
         private static readonly char[] InvalidChars = Path.GetInvalidFileNameChars();
         
+        /// <summary>
+        /// Extension method to join strings with separator but skipping invalid filename chars.
+        /// </summary>
+        /// <param name="joinThese">Array of strings to join.</param>
+        /// <param name="separator">Separator to be inserted between strings.</param>
+        /// <returns>Joined strings for usage as a filename.</returns>
         public static string JoinForFilePath(this string[] joinThese, string separator = " - ")
         {
             return new string(string.Join(separator, joinThese).Where(c => !InvalidChars.Contains(c)).ToArray());
