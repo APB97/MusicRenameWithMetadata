22898263db5e1b1cd9e0d63f5e22de843ed6e910
Adrian Bieliński <adrian.bielinski.pl@gmail.com>
2020-07-13 18:23:35 +0200

    Added IStringProcessor parameter to RenameSingle and RenameMultiple in MetadataRename.cs Imported skip.txt with common words to skip during processing Updated Program.cs Main method to be async Task and passed SkipCommonWordsProcessor instance

--- FileMetadata/Dynamic/MetadataRename.cs
+++ FileMetadata/Dynamic/MetadataRename.cs
@@ -3,28 +3,31 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using FileMetadata.Extensions;
+using StringProcessor;
 
 namespace FileMetadata.Dynamic
 {
-    public class MetadataRename
+    public static class MetadataRename
     {
-        public static void RenameMultiple(Dictionary<dynamic, Dictionary<string, string>> filePropertiesMap)
+        public static void RenameMultiple(Dictionary<dynamic, Dictionary<string, string>> filePropertiesMap,
+            IStringProcessor processor)
         {
             foreach (var (file, properties) in filePropertiesMap)
             {
-                RenameSingle(file, properties);
+                RenameSingle(file, properties, processor);
             }
         }
 
-        public static void RenameSingle(dynamic folderItem, Dictionary<string, string> properties)
+        public static void RenameSingle(dynamic folderItem, Dictionary<string, string> properties, IStringProcessor processor)
         {
             string path = Shell.Path(folderItem);
             string extension = Path.GetExtension(path);
             string joinedNoIllegalCharacters = properties.Values.ToArray().JoinForFilePath();
+            string additionallyProcessed = processor.Process(joinedNoIllegalCharacters);
             try
             {
                 File.Move(path, Path.Combine(Path.GetDirectoryName(path) ?? throw new Exception("path is not correct."),
-                    $"{joinedNoIllegalCharacters}{extension}"));
+                    $"{additionallyProcessed}{extension}"));
             }
             catch (Exception e)
             {
--- MusicMetadataRenamer/Program.cs
+++ MusicMetadataRenamer/Program.cs
@@ -1,19 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 using FileMetadata.Dynamic;
+using StringProcessor.SkipCommonWords;
 
 namespace MusicMetadataRenamer
 {
     class Program
     {
-        private static void Main(string[] args)
+        private static string _skipTxtFile = "skip.txt";
+
+        private static async Task Main(string[] args)
         {
             if (args.Length == 0) Console.WriteLine("Enter full directory path:");
             string directoryWithMusic = args.Length > 0 ? args[0] : Console.ReadLine();
             var items = Shell.GetFolderItems(directoryWithMusic);
             Dictionary<dynamic, Dictionary<string, string>> propertiesMap =
                 Metadata.GetProperties(items, new[] {PropertyNames.Artists, PropertyNames.Title});
-            MetadataRename.RenameMultiple(propertiesMap);
+            MetadataRename.RenameMultiple(propertiesMap, new SkipCommonWordsProcessor { CommonWords = new HashSet<string>(await File.ReadAllLinesAsync(_skipTxtFile))});
             Console.WriteLine("Operation completed. Press [Enter] to exit.");
             Console.ReadLine();
         }
