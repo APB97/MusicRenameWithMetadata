662b7b7c927c6b4aa2a8b5142359370a93f306e8
Adrian Bieliński <adrian.bielinski.pl@gmail.com>
2020-07-13 18:20:01 +0200

    Add xml summaries in Shell.cs

--- FileMetadata/Dynamic/Shell.cs
+++ FileMetadata/Dynamic/Shell.cs
@@ -5,6 +5,12 @@ namespace FileMetadata.Dynamic
 {
     public static class Shell
     {
+        /// <summary>
+        /// Returns instance of NameSpace (folder) for later use.
+        /// </summary>
+        /// <param name="shell">Shell instance.</param>
+        /// <param name="folderPath">Path to directory.</param>
+        /// <returns>Instance of NameSpace.</returns>
         public static dynamic NameSpace(dynamic shell, string folderPath)
         {
             Type type = shell.GetType();
@@ -12,12 +18,23 @@ namespace FileMetadata.Dynamic
                 new object[] {folderPath});
         }
 
+        /// <summary>
+        /// Returns collection of FolderItems in a NameSpace (Folder).
+        /// </summary>
+        /// <param name="folder">Folder instance. Call <see cref="NameSpace"/> to obtain the instance.</param>
+        /// <returns>Collection of FolderItems</returns>
         public static dynamic Items(dynamic folder)
         {
             Type type = folder.GetType();
             return type.InvokeMember(nameof(Items), BindingFlags.InvokeMethod, null, folder, new object[] { });
         }
 
+        /// <summary>
+        /// Get extended property of a folder item by its full name.
+        /// </summary>
+        /// <param name="item">A folder item.</param>
+        /// <param name="property">Full property name.</param>
+        /// <returns>Value of the property.</returns>
         public static dynamic ExtendedProperty(dynamic item, string property)
         {
             Type type = item.GetType();
@@ -26,23 +43,39 @@ namespace FileMetadata.Dynamic
                     property
                 });
         }
-
-        public static string Path(dynamic item)
+        
+        /// <summary>
+        /// Get full path to the given FolderItem. 
+        /// </summary>
+        /// <param name="folderItem">FolderItem to get its path.</param>
+        /// <returns>Path to FolderItem.</returns>
+        public static string Path(dynamic folderItem)
         {
-            Type type = item.GetType();
-            return type.InvokeMember(nameof(Path), BindingFlags.GetProperty, null, item, new object[] { });
+            Type type = folderItem.GetType();
+            return type.InvokeMember(nameof(Path), BindingFlags.GetProperty, null, folderItem, new object[] { });
         }
-
+        
+        /// <summary>
+        /// Get FolderItems of given folder Path.
+        /// </summary>
+        /// <param name="folderPath">Path to folder to get FolderItems from.</param>
+        /// <returns>Enumerable collection of FolderItems.</returns>
         public static dynamic GetFolderItems(string folderPath)
         {
-            var shellAppType = Type.GetTypeFromProgID("Shell.Application");
-            if (shellAppType == null) return null;
-            dynamic shellApp = Activator.CreateInstance(shellAppType);
+            var shellApp = CreateShellAppInstance();
             if (shellApp == null) return null;
             dynamic folder = NameSpace(shellApp, folderPath);
             if (folder == null) return null;
             var folderItems = Items(folder);
             return folderItems;
+        }
+        
+        private static dynamic CreateShellAppInstance()
+        {
+            var shellAppType = Type.GetTypeFromProgID("Shell.Application");
+            if (shellAppType == null) return null;
+            dynamic shellApp = Activator.CreateInstance(shellAppType);
+            return shellApp;
         }
     }
 }
