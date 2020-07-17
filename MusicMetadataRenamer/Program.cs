541395e92250c9b9bc74e6a893b83b50edb69ccc
Adrian Bieliński <adrian.bielinski.pl@gmail.com>
2020-07-17 10:17:30 +0200

    Updated Program.cs with property selection logic Added CopyIfNewer setting for skip.txt in MusicMetadataRenamer.csproj Updated .gitignore

--- MusicMetadataRenamer/Program.cs
+++ MusicMetadataRenamer/Program.cs
@@ -15,12 +15,47 @@ namespace MusicMetadataRenamer
         {
             if (args.Length == 0) Console.WriteLine("Enter full directory path:");
             string directoryWithMusic = args.Length > 0 ? args[0] : Console.ReadLine();
+
+            string[] properties = ChooseProperties();
+
             var items = Shell.GetFolderItems(directoryWithMusic);
             Dictionary<dynamic, Dictionary<string, string>> propertiesMap =
-                Metadata.GetProperties(items, new[] {PropertyNames.Artists, PropertyNames.Title});
+                Metadata.GetProperties(items, properties);
             MetadataRename.RenameMultiple(propertiesMap, new SkipCommonWordsProcessor { CommonWords = new HashSet<string>(await File.ReadAllLinesAsync(_skipTxtFile))});
+            Console.WriteLine();
             Console.WriteLine("Operation completed. Press [Enter] to exit.");
             Console.ReadLine();
+        }
+
+        private static string[] ChooseProperties()
+        {
+            List<string> chosenProperties = new List<string>();
+
+            while (chosenProperties.Count < 2)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Choose one option from 1 to 3:");
+                Console.WriteLine("1 - select Artists");
+                Console.WriteLine("2 - select Title");
+                Console.WriteLine("3 - end selection");
+
+                char input = Console.ReadKey().KeyChar;
+                switch (input)
+                {
+                    case '1':
+                        chosenProperties.Add(PropertyNames.Artists);
+                        break;
+                    case '2':
+                        chosenProperties.Add(PropertyNames.Title);
+                        break;
+                    case '3':
+                        return chosenProperties.Count != 0 ? chosenProperties.ToArray() : new[] {PropertyNames.Title};
+                    default:
+                        return new[] {PropertyNames.Title};
+                }
+            }
+            
+            return chosenProperties.ToArray();
         }
     }
 }
