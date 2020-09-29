667c0f4902e919fa9f28b82462e9f7ecd74feeb0
Adrian Bieliński <adrian.bielinski.pl@gmail.com>
2020-09-29 09:06:16 +0200

    Added ConsoleWrapper.cs with Silent property.

---
+++ MusicMetadataRenamer/ConsoleWrapper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MusicMetadataRenamer
+{
+    public class ConsoleWrapper
+    {
+        private bool Silent { get; set; }
+
+        public void WriteLine()
+        {
+            if (Silent) return;
+            Console.WriteLine();
+        }
+
+        public void WriteLine(string text)
+        {
+            if (Silent) return;
+            Console.WriteLine(text);
+        }
+
+        public void Clear()
+        {
+            Console.Clear();
+        }
+
+        public void WriteLine(object value)
+        {
+            if (Silent) return;
+            Console.WriteLine(value);
+        }
+        
+        public void BeSilent()
+        {
+            Silent = true;
+        }
+
+        public void DontBeSilent()
+        {
+            Silent = false;
+        }
+    }
+}
