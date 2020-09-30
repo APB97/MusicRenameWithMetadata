2ebcc0061436e177168f2ac9028547450da40001
Adrian Bieliński <adrian.bielinski.pl@gmail.com>
2020-09-30 12:11:26 +0200

    Added comments to SkipFile.cs. Added option to change skip file's path more than once during Prompt.

--- MusicMetadataRenamer/SkipFile.cs
+++ MusicMetadataRenamer/SkipFile.cs
@@ -17,17 +17,26 @@ namespace MusicMetadataRenamer
 
         public string SelectedPath { get; private set; } = "skip.txt";
 
+        /// <summary>
+        /// Select new path for the skip file.
+        /// </summary>
+        /// <param name="selectedPath">New skip file's path.</param>
         [SuppressMessage("ReSharper", "UnusedMember.Global")]
         public void Select(string[] selectedPath)
         {
+            // Return in case of null or empty selectedPath
             if (selectedPath == null || selectedPath.Length == 0)
                 return;
             
+            // Perform desired change
             _console.WriteLine($"Old skip file: {SelectedPath}");
             SelectedPath = selectedPath.First();
             _console.WriteLine($"New skip file: {SelectedPath}");
         }
 
+        /// <summary>
+        /// Perform interactive selection
+        /// </summary>
         public void Prompt()
         {
             Console.WriteLine("Enter the skip file path to use or press Enter to use the default:");
@@ -39,7 +48,7 @@ namespace MusicMetadataRenamer
                 if (!File.Exists(file))
                     continue;
                 Select(new [] { file });
-                break;
+                Console.WriteLine("Press Enter once more to confirm or enter different path:");
             }
         }
     }
