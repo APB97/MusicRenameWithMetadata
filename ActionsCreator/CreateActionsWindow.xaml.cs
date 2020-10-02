a943273ae74f3f86522981693169fbdd5c3dc1d9
Adrian Bieliński <adrian.bielinski.pl@gmail.com>
2020-10-02 10:00:02 +0200

    Added ActionsCreator.csproj.

---
+++ ActionsCreator/CreateActionsWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Windows.Controls;
+using CommandClassInterface;
+using Console;
+using Rename.Helpers;
+
+namespace ActionsCreator
+{
+    public partial class CreateActionsWindow
+    {
+        private IList _actionClass;
+        
+        public CreateActionsWindow()
+        {
+            InitializeComponent();
+            ConsoleWrapper consoleWrapper = new ConsoleWrapper();
+            ComboBoxActionClass.Items.Add(consoleWrapper);
+            ComboBoxActionClass.Items.Add(new PropertySelector(consoleWrapper));
+            ComboBoxActionClass.Items.Add(new DirectorySelector(consoleWrapper));
+            ComboBoxActionClass.Items.Add(new SkipFile(consoleWrapper));
+        }
+
+        private void ComboBoxActionClass_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            TextBlockDescription.Text = string.Empty;
+            ComboBoxActionName.Items.Clear();
+            _actionClass = e.AddedItems;
+            foreach (object addedItem in _actionClass)
+            {
+                if (!(addedItem is ICommandClass commandClass)) continue;
+                foreach (string command in commandClass.CommandsForJson)
+                {
+                    ComboBoxActionName.Items.Add(command);
+                }
+            }
+        }
+
+        private void ComboBoxActionName_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            foreach (object actionClassObject in _actionClass)
+            {
+                if (!(actionClassObject is ICommandClass commandClass)) continue;
+                foreach (object addedItem in e.AddedItems)
+                {
+                    string command = addedItem as string;
+                    TextBlockDescription.Text = commandClass.GetHelpFor(command);
+                }
+            }
+        }
+    }
+}
---
+++ ActionsCreator/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace ActionsCreator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private CreateActionsWindow _actionsCreation;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonCreate_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (_actionsCreation == null || !_actionsCreation.IsLoaded)
+                _actionsCreation = new CreateActionsWindow();
+            _actionsCreation.Show();
+        }
+    }
+}
