100ac40ccde1fe6bea1fc9b3ebd44890310d6dea
Adrian Bieliński <adrian.bielinski.pl@gmail.com>
2020-10-07 22:53:24 +0200

    Added Id3InfoReaderTests.cs.

---
+++ Tests/FileMetadata.Tests/Id3InfoReaderTests.cs
@@ -0,0 +1,52 @@
+using System.IO;
+using FileMetadata.Mp3;
+using NUnit.Framework;
+
+namespace FileMetadata.Tests
+{
+    public static class Id3InfoReaderTests
+    {
+        public class ReadInfoByPattern
+        {
+            private const string FileBeginning = "irrelevant values go here";
+            private const string RestOfTheContents = "rest of the contents goes here";
+
+            [Test]
+            public void GivenTextReader_WhenTitleExists_ItIsFound()
+            {
+                string testTitle = nameof(GivenTextReader_WhenTitleExists_ItIsFound);
+                var reader = new StringReader(FileBeginning +
+                                              $"{Mp3InfoReader.TitleIdSearchPattern}" +
+                                              "      " +
+                                              $"{SpecialChars.ETX}{testTitle}{SpecialChars.NullChar}" + 
+                                              RestOfTheContents);
+
+
+                string valueRead = Id3InfoReader.ReadInfoByPattern(reader, Mp3InfoReader.TitleIdSearchPattern);
+                Assert.AreEqual(testTitle, valueRead);
+            }
+
+            [Test]
+            public void GivenTextReader_WhenTitleIsNotThere_ItIsNotFound()
+            {
+                var reader = new StringReader(FileBeginning + 
+                                              SpecialChars.NullChar +
+                                              SpecialChars.NullChar +
+                                              SpecialChars.NullChar +
+                                              SpecialChars.ETX +
+                                              RestOfTheContents);
+
+                string valueRead = Id3InfoReader.ReadInfoByPattern(reader, Mp3InfoReader.TitleIdSearchPattern);
+                Assert.AreEqual(string.Empty, valueRead);
+            }
+
+            [Test]
+            public void GivenTextReader_WhenItIsEmpty_TitleIsNotFound()
+            {
+                var reader = new StringReader(string.Empty);
+                string valueRead = Id3InfoReader.ReadInfoByPattern(reader, Mp3InfoReader.TitleIdSearchPattern);
+                Assert.AreEqual(string.Empty, valueRead);
+            }
+        }
+    }
+}
