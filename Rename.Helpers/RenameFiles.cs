13a2722de778298fac7f9085090e0883d1f85e26
Adrian Bieliński <adrian.bielinski.pl@gmail.com>
2020-10-08 05:23:11 +0200

    Added xml documentation to RenameFiles.cs.

--- Rename.Helpers/RenameFiles.cs
+++ Rename.Helpers/RenameFiles.cs
@@ -9,12 +9,21 @@ using StringProcessor.DefaultNone;
 
 namespace Rename.Helpers
 {
+    /// <summary>
+    /// Class for renaming multiple files in multiple directories.
+    /// </summary>
     public class RenameFiles
     {
         private readonly IConsole _silenceAbleConsole;
         private readonly IStringProcessor _wordProcessor;
         private readonly MetadataRename _metadataRename;
 
+        /// <summary>
+        /// Creates new instance of RenameFiles passing dependencies.
+        /// </summary>
+        /// <param name="silenceAbleConsole">Console to use for output.</param>
+        /// <param name="wordProcessor">Optional parameter - can be null to use no additional word processing.</param>
+        /// <param name="metadataRename">Instance of MetadataRename class to use for renaming.</param>
         public RenameFiles(IConsole silenceAbleConsole, IStringProcessor wordProcessor, MetadataRename metadataRename)
         {
             _silenceAbleConsole = silenceAbleConsole ?? throw new ArgumentNullException(nameof(silenceAbleConsole));
@@ -22,6 +31,12 @@ namespace Rename.Helpers
             _metadataRename = metadataRename ?? throw new ArgumentNullException(nameof(metadataRename));
         }
 
+        /// <summary>
+        /// Renames multiple files.
+        /// </summary>
+        /// <param name="directoriesToProcess">Directories to include in operation.</param>
+        /// <param name="propertiesToUse">Properties to use for renaming.</param>
+        /// <param name="allDirectoriesOrBaseOnly">Should all or only top directory be processed?</param>
         public void RenameMultiple(IEnumerable<string> directoriesToProcess, IEnumerable<string> propertiesToUse,
             SearchOption allDirectoriesOrBaseOnly = SearchOption.AllDirectories)
         {
